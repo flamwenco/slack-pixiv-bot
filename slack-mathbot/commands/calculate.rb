7e090066ceb3abefa173ceeb398fd700fbee20a6
Daniel Muckerman <danielmuckerman@me.com>
2015-10-09 13:32:32 -0400

    Should fucking remove gif spam

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -10,10 +10,10 @@ module SlackMathbot
         if result && result.length > 0
           send_message client, data.channel, result
         else
-          send_message_with_gif client, data.channel, 'Got nothing.', 'nothing'
+          send_message client, data.channel, 'Got nothing.', 'nothing'
         end
       rescue StandardError => e
-        send_message_with_gif client, data.channel, "Sorry, #{e.message}.", 'idiot'
+        send_message client, data.channel, "Sorry, #{e.message}.", 'idiot'
       end
     end
   end
