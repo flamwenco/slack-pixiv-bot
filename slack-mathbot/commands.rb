82ba015700e9ae2c09c36754611c78c369a28307
Daniel Muckerman <danielmuckerman@me.com>
2015-10-09 18:26:23 -0400

    Maybe this time?

--- slack-mathbot/commands.rb
+++ slack-mathbot/commands.rb
@@ -1,4 +1,4 @@
 require 'slack-mathbot/commands/calculate'
+require 'slack-mathbot/commands/echo'
 require 'slack-mathbot/commands/about'
 require 'slack-mathbot/commands/help'
-require 'slack-mathbot/commands/echo'
