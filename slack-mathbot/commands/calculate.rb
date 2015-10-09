b6ce19882f5b288875012fd8331c91c3dab43653
Daniel Muckerman <danielmuckerman@me.com>
2015-10-09 18:32:45 -0400

    Remove rules check. Live dangerously

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -5,7 +5,8 @@ module SlackMathbot
       command 'calculate'
 
       def self.call(client, data, match)
-        result = Dentaku::Calculator.new.evaluate(match[:expression]) if match.names.include?('expression')
+        result = Dentaku::Calculator.new.evaluate(match[:expression])
+		#if match.names.include?('expression')
         result = result.to_s if result
         if result && result.length > 0
           send_message client, data.channel, result
