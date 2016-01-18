c2519b3376342c4d90655b73bb062ecbd5f11a36
Daniel Muckerman <danielmuckerman@me.com>
2016-01-17 22:50:16 -0500

    Test attachments

--- slack-pixiv/commands/pixiv.rb
+++ slack-pixiv/commands/pixiv.rb
@@ -5,7 +5,9 @@ module SlackMathbot
       match /pixiv\.net(?<url>.*)$/ do |client, _data, _match|
 
         #asdf
-        client.say(channel: _data.channel, text: "Testing: http://pixiv.net#{_match[:url]}")
+        puts _match[:url]
+        #client.say(channel: _data.channel, text: "Testing: http://pixiv.net#{_match[:url]}")
+        client.chat_postMessage(channel: _data.channel, attachments: "[{'pretext': 'pre-hello', 'text': 'text-world'}]")
         #send_message client, _data.channel, "Testing: #{_match[:url]}"
 
       end
