c8c82899760c64ad5aaa2500fbb5367b118622e9
Daniel Muckerman <danielmuckerman@me.com>
2016-01-17 22:36:59 -0500

    Testing a thing

--- slack-pixiv/commands/pixiv.rb
+++ slack-pixiv/commands/pixiv.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
 module SlackMathbot
   module Commands
-    class Pixiv < SlackRubyBot::Commands::Base
+    class Pixiv < SlackRubyBot::Bot
       match /pixiv\.net(?<url>.*)$/ do |client, _data, _match|
 
         #asdf
-        #client.say(channel: _data.channel, text: "Testing: #{_match[:url]}")
-        send_message client, _data.channel, "Testing: #{_match[:url]}"
+        client.say(channel: _data.channel, text: "Testing: http://pixiv.net#{_match[:url]}")
+        #send_message client, _data.channel, "Testing: #{_match[:url]}"
 
       end
     end
