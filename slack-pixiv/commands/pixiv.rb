7a29e4141a7ac4b0313adb594e8a54cf1be99542
Ryan Hammett <rhammett@ycp.edu>
2016-02-01 15:34:02 -0500

    Testing illustration logic

--- slack-pixiv/commands/pixiv.rb
+++ slack-pixiv/commands/pixiv.rb
@@ -12,10 +12,13 @@ module SlackMathbot
         puts pixiv_url
 
         # Create iOS Illustration URL
-        ios_url = "pixiv://illusts/" + pixiv_url.split('=')[-1]
-        puts ios_url
+        # ios_url = "pixiv://illusts/" + pixiv_url.split("illust_id=")[-1]
+	illust_url = pixiv_url.split("illust_id=")[-1]
+	puts illust_url
+	ios_url = "pixiv://illusts/" + illust_url.slice(^[0-9]+$)
+       	puts ios_url
 
-	# TODO: Create iOS Member URL, find correct URL scheme
+	# TODO: Create iOS Member URL, must find correct URL scheme
 	ios_mem_url = "pixiv://member/" + pixiv_url.split("?id=")[-1]
 	puts ios_mem_url
 
